659087836a332d1214e9731859bd8e56ab155c28
kulakovskyi <torchok9@gmail.com>
2022-12-13 18:55:26 +0200

    add desc 1920x1080

--- front/js/main.js
+++ front/js/main.js
@@ -52,3 +52,120 @@ const renderElements = (matches) => {
         rightShirt.src = "img/" + currentMatch.shirtRight;
     }
 }
+
+function GenerateSnow(options) {
+    const self = this;
+    let interval;
+    const defaults = {
+        elem: '',
+        quantity: 100,
+        snowFlakeClass: '',
+    };
+    let _snowFlakes = [];
+    let _lastPositions = [];
+
+    const _snowingTurn = function () {
+
+        for (let i = 0; i < _lastPositions.length; i++) {
+            (function (k) {
+                const newPosX = _lastPositions[k].left + (Math.ceil(Math.random() * 2 - 1.5));
+                let newPosY = _lastPositions[k].top + Math.ceil(Math.random() * 4);
+                let snowFlake = _snowFlakes[k];
+                if (newPosY > 113) {
+                    newPosY = -1;
+                    snowFlake.classList.add('no-animate');
+                } else {
+                    snowFlake.classList.remove('no-animate');
+                }
+                _lastPositions[k] = {
+                    left: newPosX,
+                    top: newPosY
+                }
+
+            })(i);
+        }
+        _refreshSnowFlakes();
+    };
+
+    const _refreshSnowFlakes = function () {
+        for (let i = 0; i < _lastPositions.length; i++) {
+            (function (k) {
+                const snowFlake = _snowFlakes[k];
+                const newPos = _lastPositions[k];
+                snowFlake.style.top = newPos.top + '%';
+                snowFlake.style.left = newPos.left + '%';
+            })(i);
+        }
+    };
+
+    const _letItSnow = function () {
+        interval = setInterval(function () {
+            _snowingTurn();
+        }, 700);
+    };
+
+    self.settings = {};
+
+    const refreshSettings = function () {
+        const optionsKeys = Object.keys(options);
+        Object.keys(defaults).forEach(function (key) {
+            self.settings[key] = optionsKeys.indexOf(key) > -1 ? options[key] : defaults[key];
+        });
+    };
+
+    self.clear = function () {
+        _lastPositions = [];
+        const wrapper = self.elem.querySelector('.snowflakes-wrapper');
+        wrapper && wrapper.remove();
+        interval && clearInterval(interval);
+    };
+
+    self.init = function () {
+        refreshSettings();
+        self.elem = self.settings.elem;
+        self.clear();
+        generateSnowflakes();
+        _letItSnow();
+    };
+
+    const generateSnowflakes = function () {
+        refreshSettings();
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('snowflakes-wrapper');
+        _lastPositions = [];
+        for (let i = 0; i < self.settings.quantity; i++) {
+            (function () {
+                const snowflake = document.createElement('div');
+                snowflake.classList.add('snowflake');
+                self.settings.snowFlakeClass && snowflake.classList.add(self.settings.snowFlakeClass);
+                const posX = Math.random() * 100;
+                const posY = Math.random() * 104 - 4;
+                const size = Math.ceil(Math.random() * 7) + 2;
+                _lastPositions.push({
+                    top: posY,
+                    left: posX
+                });
+                snowflake.style.height = size + 'px';
+                snowflake.style.width = size + 'px';
+                wrapper.appendChild(snowflake);
+            })();
+        }
+        self.elem.appendChild(wrapper);
+        _snowFlakes = self.elem.querySelectorAll('.snowflake');
+        _refreshSnowFlakes();
+    };
+    self.stop = function () {
+        clearInterval(interval);
+    };
+
+    self.init();
+
+    return self;
+}
+(function() {
+    const kek = new GenerateSnow({
+        elem: document.getElementById('snow-section'),
+        quantity: 60,
+        snowFlakeClass: 'snowflake-main',
+    });
+})()
